5157f98af766f0bfb9e20e29eac6123e76e57c7d
pavelangelov <p_a_v_el@abv.bg>
2017-03-05 13:57:50 +0200

    Add validator

---
+++ Bg-Fishing/Bg-Fishing.Utils/Validator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bg_Fishing.Utils
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Validate if passed value is null.
+        /// </summary>
+        /// <param name="value">Value to validate</param>
+        /// <param name="paramName">Parameter name.</param>
+        /// <param name="errorMessage">Error message.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void ValidateForNull(object value, string paramName = null, string errorMessage = null)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName, errorMessage);
+            }
+        }
+    }
+}
