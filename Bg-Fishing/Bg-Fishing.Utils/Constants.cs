0cf9454c6b3110980215dcdaf7a5e00289927021
pavelangelov <p_a_v_el@abv.bg>
2017-03-05 13:58:52 +0200

    Add class to hold constant values for the app.

---
+++ Bg-Fishing/Bg-Fishing.Utils/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bg_Fishing.Utils
+{
+    public static class Constants
+    {
+    }
+}
