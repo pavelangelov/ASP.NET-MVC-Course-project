d6c1bb3c8be67e505570b838c828ff3c5adb1a9c
pavelangelov <p_a_v_el@abv.bg>
2017-03-05 14:01:10 +0200

    Started services

--- Bg-Fishing/Bg-Fishing.Services/Services/UserService.cs
+++ Bg-Fishing/Bg-Fishing.Services/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bg_Fishing.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Bg_Fishing.Services
 {
-    public class Class1
+    public class UserService : IUserService
     {
     }
 }
