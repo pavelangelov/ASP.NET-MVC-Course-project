16e616752583666b38f7e146fbf2ff2bb77fcde0
pavelangelov <p_a_v_el@abv.bg>
2017-03-06 11:29:21 +0200

    Add controller for managing videos.

---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Controllers/Moderator/ManageVideosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bg_Fishing.MvcClient.Controllers.Moderator
+{
+    public class ManageVideosController : Controller
+    {
+        // GET: ManageVideos
+        [Authorize(Roles = "Moderator,Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add(string url)
+        {
+            if (url == null || url.Length == 0)
+            {
+                return Json(new { status = "error", message = "Видеото не може да бъде добавено." }); ;
+            }
+
+            try
+            {
+                // Add video to Gallery.
+                return Json(new { status = "success", message = "Видеото е добавено." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = "error", message = "Видеото не може да бъде добавено." });
+            }
+        }
+    }
+}
