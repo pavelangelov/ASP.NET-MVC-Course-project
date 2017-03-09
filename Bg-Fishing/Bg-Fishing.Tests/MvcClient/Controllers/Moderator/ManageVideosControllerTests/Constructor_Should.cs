592c37a36a9fbf5fb1bf5071a385315937901284
pavelangelov <p_a_v_el@abv.bg>
2017-03-09 15:13:10 +0200

    Add unit tests.

---
+++ Bg-Fishing/Bg-Fishing.Tests/MvcClient/Controllers/Moderator/ManageVideosControllerTests/Constructor_Should.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.MvcClient.Controllers.Moderator;
+using Bg_Fishing.Services.Contracts;
+
+namespace Bg_Fishing.Tests.MvcClient.Controllers.Moderator.ManageVideosControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_IfPassedVideoService_IsNull()
+        {
+            // Arrange, Act & Assert
+            var message = Assert.Throws<ArgumentNullException>(() => new ManageVideosController(null)).Message;
+            StringAssert.Contains("videoService", message);
+        }
+
+        [Test]
+        public void NotToThrow_IfVideoService_IsNotNull()
+        {
+            // Arrange
+            var mockedService = new Mock<IVideoService>();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => new ManageVideosController(mockedService.Object));
+        }
+    }
+}
---
+++ Bg-Fishing/Bg-Fishing.Tests/MvcClient/Controllers/Moderator/ManageVideosControllerTests/GetIndex_Should.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.MvcClient.Controllers.Moderator;
+using Bg_Fishing.MvcClient.Models.ViewModels.Moderator;
+using Bg_Fishing.Services.Contracts;
+
+namespace Bg_Fishing.Tests.MvcClient.Controllers.Moderator.ManageVideosControllerTests
+{
+    [TestFixture]
+    public class GetIndex_Should
+    {
+        [Test]
+        public void GetAllGalleryNames_AndStoreItToViewModel()
+        {
+            // Arrange
+            var galleryNames = new string[] { "Test", "Test2" };
+            var mockedService = new Mock<IVideoService>();
+            mockedService.Setup(s => s.GetAll()).Returns(galleryNames).Verifiable();
+            var controller = new ManageVideosController(mockedService.Object);
+
+            // Act
+            var view = controller.Index() as ViewResult;
+            var model = view.ViewData.Model as AddVideoViewModel;
+
+            // Assert
+            Assert.IsAssignableFrom<AddVideoViewModel>(model);
+            Assert.IsTrue(model.GalleryNames.Count() == 3);
+            Assert.IsTrue(model.GalleryNames.Last().Value == "Test2");
+        }
+    }
+}
