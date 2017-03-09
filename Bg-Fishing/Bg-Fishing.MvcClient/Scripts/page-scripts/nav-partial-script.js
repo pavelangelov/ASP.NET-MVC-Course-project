32c28bd8966e4e2f47d3dd906f4d9f9cf353666f
pavelangelov <p_a_v_el@abv.bg>
2017-03-09 20:32:56 +0200

    Move nav scripts in separated file

---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Scripts/page-scripts/nav-partial-script.js
@@ -0,0 +1,13 @@
+﻿// Collapse accordion every time dropdown is shown
+$('.dropdown-accordion').on('show.bs.dropdown', function (event) {
+    var accordion = $(this).find($(this).data('accordion'));
+    accordion.find('.panel-collapse.in').collapse('hide');
+});
+
+// Prevent dropdown to be closed when we click on an accordion link
+$('.dropdown-accordion').on('click', 'a[data-toggle="collapse"]', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
+    $($(this).attr('href')).collapse('show');
+})
