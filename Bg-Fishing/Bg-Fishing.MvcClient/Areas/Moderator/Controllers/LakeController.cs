a172f1a853475e0527d062b5d92325da1870ef97
pavelangelov <p_a_v_el@abv.bg>
2017-03-17 19:19:44 +0200

    Started.

---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Areas/Moderator/Controllers/LakeController.cs
@@ -0,0 +1,33 @@
+﻿using Bg_Fishing.MvcClient.Areas.Moderator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bg_Fishing.MvcClient.Areas.Moderator.Controllers
+{
+    public class LakeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(LakeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Add new lake
+                return Json(new { status = "success", message = "Язовира е добавен успешно" });
+            }
+            else
+            {
+                var errors = string.Join(" ", ModelState.Values);
+                return Json(new { status = "error", message = errors });
+            }
+        }
+    }
+}
---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Areas/Moderator/Models/LakeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bg_Fishing.MvcClient.Areas.Moderator.Models
+{
+    public class LakeViewModel
+    {
+        [Display(Name = "Име")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string LocationId { get; set; }
+    }
+}
