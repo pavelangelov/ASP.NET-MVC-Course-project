525b7034c51738778745a05f13986f628c8e55ed
pavelangelov <p_a_v_el@abv.bg>
2017-03-17 21:04:44 +0200

    Up

--- Bg-Fishing/Bg-Fishing.MvcClient/Areas/Moderator/Controllers/LocationController.cs
+++ Bg-Fishing/Bg-Fishing.MvcClient/Areas/Moderator/Controllers/LocationController.cs
@@ -1,4 +1,6 @@
-﻿using Bg_Fishing.MvcClient.Areas.Moderator.Models;
+﻿using Bg_Fishing.Factories.Contracts;
+using Bg_Fishing.MvcClient.Areas.Moderator.Models;
+using Bg_Fishing.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,15 @@ namespace Bg_Fishing.MvcClient.Areas.Moderator.Controllers
 {
     public class LocationController : Controller
     {
+        private ILocationFactory locationFactory;
+
+        public LocationController(ILocationFactory locationFactory)
+        {
+            Validator.ValidateForNull(locationFactory, "locationFactory");
+
+            this.locationFactory = locationFactory;
+        }
+
         [HttpGet]
         public ActionResult Add()
         {
@@ -20,6 +31,7 @@ namespace Bg_Fishing.MvcClient.Areas.Moderator.Controllers
         {
             if (ModelState.IsValid)
             {
+                var location = this.locationFactory.CreateLocation(model.Latitude, model.Longitude, model.Name, model.Info);
                 // TODO: Add new location
             }
 
