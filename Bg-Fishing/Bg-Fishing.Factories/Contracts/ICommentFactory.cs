32528fb24056ecbfbe5b8340a2a44ada14192d8c
pavelangelov <p_a_v_el@abv.bg>
2017-03-23 12:21:32 +0200

    Added CommentFactory.

---
+++ Bg-Fishing/Bg-Fishing.Factories/Contracts/ICommentFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Bg_Fishing.Models;
+
+namespace Bg_Fishing.Factories.Contracts
+{
+    public interface ICommentFactory
+    {
+        Comment CreateComment();
+
+        Comment CreateComment(string lakeName, string username, string content, DateTime postedDate);
+    }
+}
