49280bbb5d1d326fd697b419a093bad6e7aed4f2
pavelangelov <p_a_v_el@abv.bg>
2017-03-23 12:05:27 +0200

    Started add comments.

--- Bg-Fishing/Bg-Fishing.MvcClient/Controllers/LakesController.cs
+++ Bg-Fishing/Bg-Fishing.MvcClient/Controllers/LakesController.cs
@@ -1,10 +1,8 @@
-﻿using Bg_Fishing.Services.Contracts;
+﻿using System.Web.Mvc;
+
+using Bg_Fishing.MvcClient.Models.ViewModels;
+using Bg_Fishing.Services.Contracts;
 using Bg_Fishing.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace Bg_Fishing.MvcClient.Controllers
 {
@@ -25,6 +23,26 @@ namespace Bg_Fishing.MvcClient.Controllers
             var lake = this.lakeService.FindByName(name);
 
             return View("Index", model: lake);
+        }
+
+        [HttpGet]
+        public ActionResult Comments(string lakeName, int page)
+        {
+            var lake = this.lakeService.FindByName(lakeName);
+
+            return View(model: lake);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(AddCommentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Add new comment
+            }
+
+            // TODO: Show model errors
+            return View();
         }
     }
 }
---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Models/ViewModels/AddCommentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bg_Fishing.MvcClient.Models.ViewModels
+{
+    public class AddCommentViewModel
+    {
+        public string LakeName { get; set; }
+
+        public string Username { get; set; }
+
+        public string Content { get; set; }
+    }
+}
