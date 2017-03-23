7e2d72fd1ed233ce44837d03cd44c9a01dd2cc46
pavelangelov <p_a_v_el@abv.bg>
2017-03-23 11:52:19 +0200

    Move scripts.

---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Scripts/page-scripts/show-location.js
@@ -0,0 +1,8 @@
+﻿function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(lat, long),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
