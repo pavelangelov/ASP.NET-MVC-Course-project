0bcff1e349e8ff711098eaf20877eacfd70043fb
pavelangelov <p_a_v_el@abv.bg>
2017-03-25 18:43:10 +0200

    Added NewsComment unit tests.

---
+++ Bg-Fishing/Bg-Fishing.Tests/Models/NewsCommentTests/Constructor_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Bg_Fishing.Models;
+
+namespace Bg_Fishing.Tests.Models.NewsCommentTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void NotThrow_IfCalledParameterlessConstructor()
+        {
+            // Arrange, Act
+            var comment = new NewsComment();
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, comment.Id);
+        }
+
+        [Test]
+        public void NotThrow_IfAllParametersAreValid_AndSetCorrectValues()
+        {
+            // Arrange
+            var username = "test user";
+            var content = "test content";
+            var date = DateTime.UtcNow;
+
+            // Act
+            var comment = new NewsComment(content, username, date);
+
+            // Assert
+            Assert.AreEqual(content, comment.Content);
+            Assert.AreEqual(username, comment.Username);
+            Assert.AreEqual(date, comment.PostedOn);
+        }
+    }
+}
