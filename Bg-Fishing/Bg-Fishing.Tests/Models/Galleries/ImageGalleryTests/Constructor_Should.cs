837d621e65dd827b2e2ce61f9207ff48c8ff85a6
pavelangelov <p_a_v_el@abv.bg>
2017-03-25 19:45:28 +0200

    Added unit tests for ImageGallery.

---
+++ Bg-Fishing/Bg-Fishing.Tests/Models/Galleries/ImageGalleryTests/Constructor_Should.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Bg_Fishing.Models.Galleries;
+
+namespace Bg_Fishing.Tests.Models.Galleries.ImageGalleryTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void NotThrow_IfCalledParameterlessConstructor()
+        {
+            // Arrange & Act
+            var imageGallery = new ImageGallery();
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, imageGallery.Id);
+            Assert.IsNotNull(imageGallery.Images);
+        }
+
+        [Test]
+        public void NotThrow_AndSetCorrectValues()
+        {
+            // Arrange
+            var name = "Test";
+            var lakeId = Guid.NewGuid().ToString();
+
+            // Act
+            var imageGallery = new ImageGallery(name, lakeId);
+
+            // Assert
+            Assert.AreEqual(name, imageGallery.Name);
+            Assert.AreEqual(lakeId, imageGallery.LakeId);
+        }
+    }
+}
