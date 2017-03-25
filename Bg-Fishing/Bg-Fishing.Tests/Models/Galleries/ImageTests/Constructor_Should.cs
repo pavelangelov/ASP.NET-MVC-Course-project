5b0bce34dce1ffc94f1b0790b2591d70166e951a
pavelangelov <p_a_v_el@abv.bg>
2017-03-25 19:49:46 +0200

    Added unit tests for Image.

---
+++ Bg-Fishing/Bg-Fishing.Tests/Models/Galleries/ImageTests/Constructor_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Bg_Fishing.Models.Galleries;
+
+namespace Bg_Fishing.Tests.Models.Galleries.ImageTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void NotThrow_IfCalledParameterlessConstructor()
+        {
+            // Arrange & Act
+            var image = new Image();
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, image.Id);
+        }
+
+        [Test]
+        public void NotThrow_AndSetCorrectValues()
+        {
+            // Arrange
+            var url = "Test url";
+            var date = DateTime.UtcNow;
+            var info = "Test info";
+
+            // Act
+            var image = new Image(url, date, info);
+
+            // Assert
+            Assert.AreEqual(url, image.ImageUrl);
+            Assert.AreEqual(date, image.Date);
+            Assert.AreEqual(info, image.Info);
+        }
+    }
+}
