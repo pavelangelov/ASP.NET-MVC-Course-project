92a4f866e352aac19980906ad80bdd6b93d1f4f8
pavelangelov <p_a_v_el@abv.bg>
2017-03-26 16:59:35 +0300

    Added SignalR hub to load news.

---
+++ Bg-Fishing/Bg-Fishing.MvcClient/Hubs/NewsHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Bg_Fishing.Services.Contracts;
+using Bg_Fishing.Utils;
+
+namespace Bg_Fishing.MvcClient.Hubs
+{
+    public class NewsHub : Hub
+    {
+        public const int NewsCount = 5;
+
+        private INewsService newsService;
+
+        public NewsHub(INewsService newsService)
+        {
+            Validator.ValidateForNull(newsService, paramName: "newsService");
+
+            this.newsService = newsService;
+        }
+
+        public void GetNews(int page)
+        {
+            var skip = page * NewsCount;
+            var news = this.newsService.GetNews(skip, NewsCount).ToList();
+            var hasMore = false;
+            var nextPage = 0;
+
+            var allNewsCount = this.newsService.GetNewsCount();
+            if (allNewsCount - (skip + NewsCount) > 0)
+            {
+                hasMore = true;
+                nextPage = page + 1;
+            }
+
+            Clients.All.loadNews(new { news = news, hasMore = hasMore, nextPage = nextPage });
+        }
+    }
+}
--- Bg-Fishing/Bg-Fishing.MvcClient/Scripts/page-scripts/home.js
+++ Bg-Fishing/Bg-Fishing.MvcClient/Scripts/page-scripts/home.js
@@ -1,7 +1,66 @@
 ﻿$('.container').on('click', 'a.show-more-btn', (ev) => {
     ev.preventDefault();
-    var $this = $(ev.target);
-    console.log($this.parent());
+    let $this = $(ev.target);
+
     $($this.parent()).prev().css('max-height', 'inherit');
     $this.hide();
 })
+
+$(function () {
+    var news = $.connection.newsHub;
+
+    news.client.loadNews = function (response) {
+        let container = $('#news-container');
+
+        response.news.forEach((n) => {
+            let newsContainer = $('<div />').addClass('col-sm-10')
+                                            .addClass('col-sm-offset-1');
+            let article = $('<article />').addClass('post');
+
+            let thumb = $('<div />').addClass('post-thumb');
+            let img = $('<img />').attr('src', n.ImageUrl)
+                                    .attr('alt', 'News-Image')
+                                    .appendTo(thumb);
+
+            let content = $('<div />').addClass('post-content');
+            let header = $('<header />').addClass('entry-header')
+                                        .addClass('text-center')
+                                        .addClass('text-uppercase');
+            let h1 = $('<h1 />').addClass('entry-title')
+                                .text(n.Title)
+                                .appendTo(header);
+            let entryContent = $('<div />').addClass('entry-content')
+            let p = $('<p />').text(n.Content).appendTo(entryContent);
+            let date = $('<div />').addClass('date')
+                                    .text('Публикувано на: ' + n.PostedOn)
+                                    .appendTo(entryContent);
+
+            header.appendTo(content);
+            entryContent.appendTo(content);
+            $('<div />').addClass('show-more')
+                        .html('<a class="btn show-more-btn">Виж повече...</a>')
+                        .appendTo(content);
+
+            thumb.appendTo(article);
+            content.appendTo(article);
+
+            article.appendTo(newsContainer);
+            newsContainer.appendTo(container);
+        })
+
+        if (response.hasMore) {
+            $('a.load-more-btn').attr('data-next-page', response.nextPage);
+        } else {
+            $('div#load-more').html('');
+        }
+    };
+
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        $('.container').on('click', 'a.load-more-btn', (ev) => {
+            let page = $(ev.target).attr('data-next-page');
+
+            news.server.getNews(page);
+        });
+    });
+});
