31db8986a65b9ed56203549da61b40fe4df90509
pavelangelov <p_a_v_el@abv.bg>
2017-04-08 14:58:53 +0300

    Added InnerCommentFactory.

---
+++ Bg-Fishing/Bg-Fishing.Factories/Contracts/IInnerCommentFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Bg_Fishing.Models.Comments;
+
+namespace Bg_Fishing.Factories.Contracts
+{
+    public interface IInnerCommentFactory
+    {
+        InnerComment CreateInnerComment();
+
+        InnerComment CreateInnerComment(string content, string username, DateTime postedDate);
+    }
+}
