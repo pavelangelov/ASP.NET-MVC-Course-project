dcbc9de8014ead112ceab411625be40630367722
pavelangelov <p_a_v_el@abv.bg>
2017-05-16 14:02:49 +0300

    Added unit tests for InnerComment.

---
+++ Bg-Fishing/Bg-Fishing.Tests/Models/InnerCommentTests/Constructor_Should.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Bg_Fishing.Models.Comments;
+
+namespace Bg_Fishing.Tests.Models.InnerCommentTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void NotThrow_IfCalledParameterlessConstructor()
+        {
+            // Arrange, Act
+            var comment = new InnerComment();
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, comment.Id);
+        }
+
+        [Test]
+        public void NotThrow_IfAllParametersAreValid_AndSetCorrectValues()
+        {
+            // Arrange
+            var username = "test user";
+            var content = "test content";
+            var date = DateTime.UtcNow;
+
+            // Act
+            var comment = new InnerComment(content, username, date);
+
+            // Assert
+            Assert.AreEqual(content, comment.Content);
+            Assert.AreEqual(username, comment.Username);
+            Assert.AreEqual(date, comment.PostedDate);
+        }
+    }
+}
