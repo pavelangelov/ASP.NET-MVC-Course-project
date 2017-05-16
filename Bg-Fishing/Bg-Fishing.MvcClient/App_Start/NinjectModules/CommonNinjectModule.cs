57c14875a353af64b32a1a6cbd15b7d2eca54bf6
pavelangelov <p_a_v_el@abv.bg>
2017-05-16 13:55:00 +0300

    Update Ninject modules.

--- Bg-Fishing/Bg-Fishing.MvcClient/App_Start/NinjectModules/CommonNinjectModule.cs
+++ Bg-Fishing/Bg-Fishing.MvcClient/App_Start/NinjectModules/CommonNinjectModule.cs
@@ -1,10 +1,7 @@
-﻿using Bg_Fishing.Utils;
+﻿using Ninject.Modules;
+
+using Bg_Fishing.Utils;
 using Bg_Fishing.Utils.Contracts;
-using Ninject.Modules;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Bg_Fishing.MvcClient.App_Start.NinjectModules
 {
@@ -13,6 +10,7 @@ namespace Bg_Fishing.MvcClient.App_Start.NinjectModules
         public override void Load()
         {
             this.Bind<IDateProvider>().To<DateProvider>();
+            this.Bind<IDirectoryHelper>().To<DirectoryHelper>();
         }
     }
 }
