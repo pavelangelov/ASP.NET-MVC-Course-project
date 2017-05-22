75415cd1404095497c9b47d19710291bc3536cbf
pavelangelov <p_a_v_el@abv.bg>
2017-05-22 16:17:48 +0300

    Added new unit tests.

---
+++ Bg-Fishing/Bg-Fishing.Tests/MvcClient/Controllers/ImageControllerTests/GetConfirm_Should.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.Factories.Contracts;
+using Bg_Fishing.MvcClient.Controllers;
+using Bg_Fishing.Services.Contracts;
+using Bg_Fishing.Services.Models;
+using Bg_Fishing.Utils.Contracts;
+
+namespace Bg_Fishing.Tests.MvcClient.Controllers.ImageControllerTests
+{
+    [TestFixture]
+    public class GetConfirm_Should
+    {
+        [Test]
+        public void GetGalleriesWithunconfirmedImages_FromDatabase_AndRenderDefaultView()
+        {
+            // Arrange
+            var mockedCollection = new List<ImageGalleryModel>();
+            var mockedImageGalleryService = new Mock<IImageGalleryService>();
+            mockedImageGalleryService.Setup(s => s.GetGalleriesWithUnconfirmedImages())
+                                        .Returns(mockedCollection)
+                                        .Verifiable();
+
+            var mockedImageFactory = new Mock<IImageFactory>();
+            var mockedDateProvider = new Mock<IDateProvider>();
+            var mockedLakeService = new Mock<ILakeService>();
+            var mockedImageGalleryFactory = new Mock<IImageGalleryFactory>();
+            var mockedDirectoryHelper = new Mock<IDirectoryHelper>();
+
+            var controller = new ImageController(
+                mockedImageGalleryService.Object,
+                mockedImageFactory.Object,
+                mockedDateProvider.Object,
+                mockedLakeService.Object,
+                mockedImageGalleryFactory.Object,
+                mockedDirectoryHelper.Object);
+
+            // Act
+            var result = controller.Confirm() as ViewResult;
+            var model = result.Model as IEnumerable<ImageGalleryModel>;
+
+            // Assert
+            Assert.AreEqual(mockedCollection, model);
+            Assert.IsTrue(result.ViewName == "");
+
+            mockedImageGalleryService.Verify(s => s.GetGalleriesWithUnconfirmedImages(), Times.Once);
+        }
+    }
+}
---
+++ Bg-Fishing/Bg-Fishing.Tests/MvcClient/Controllers/ImageControllerTests/PutConfirm_Should.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.Factories.Contracts;
+using Bg_Fishing.MvcClient.Controllers;
+using Bg_Fishing.Services.Contracts;
+using Bg_Fishing.Utils.Contracts;
+
+namespace Bg_Fishing.Tests.MvcClient.Controllers.ImageControllerTests
+{
+    [TestFixture]
+    public class PutConfirm_Should
+    {
+        [Test]
+        public void ReturnError_IfConfirmingImageFailed()
+        {
+            // Arrange
+            var mockedImageGalleryService = new Mock<IImageGalleryService>();
+            mockedImageGalleryService.Setup(s => s.ConfirmImage(It.IsAny<string>()))
+                                        .Throws<NullReferenceException>();
+
+            var mockedImageFactory = new Mock<IImageFactory>();
+            var mockedDateProvider = new Mock<IDateProvider>();
+            var mockedLakeService = new Mock<ILakeService>();
+            var mockedImageGalleryFactory = new Mock<IImageGalleryFactory>();
+            var mockedDirectoryHelper = new Mock<IDirectoryHelper>();
+
+            var controller = new ImageController(
+                mockedImageGalleryService.Object,
+                mockedImageFactory.Object,
+                mockedDateProvider.Object,
+                mockedLakeService.Object,
+                mockedImageGalleryFactory.Object,
+                mockedDirectoryHelper.Object);
+
+            // Act
+            var result = controller.Confirm(It.IsAny<string>());
+
+            // Assert
+            StringAssert.Contains("error", result);
+            StringAssert.Contains("Опитвате се да потвърдите невалидно изображение!", result);
+        }
+
+        [Test]
+        public void ReturnSuccess_IfConfirmingImageNotFailed()
+        {
+            // Arrange
+            var mockedImageGalleryService = new Mock<IImageGalleryService>();
+            mockedImageGalleryService.Setup(s => s.ConfirmImage(It.IsAny<string>()))
+                                        .Verifiable();
+
+            var mockedImageFactory = new Mock<IImageFactory>();
+            var mockedDateProvider = new Mock<IDateProvider>();
+            var mockedLakeService = new Mock<ILakeService>();
+            var mockedImageGalleryFactory = new Mock<IImageGalleryFactory>();
+            var mockedDirectoryHelper = new Mock<IDirectoryHelper>();
+            
+            var controller = new ImageController(
+                mockedImageGalleryService.Object,
+                mockedImageFactory.Object,
+                mockedDateProvider.Object,
+                mockedLakeService.Object,
+                mockedImageGalleryFactory.Object,
+                mockedDirectoryHelper.Object);
+
+            var mockedImageId = "Mocked Image Id";
+
+            // Act
+            var result = controller.Confirm(mockedImageId);
+
+            // Assert
+            StringAssert.Contains("success", result);
+            StringAssert.Contains("Изображението е потвърдено", result);
+
+            mockedImageGalleryService.Verify(s => s.ConfirmImage(mockedImageId), Times.Once);
+        }
+    }
+}
