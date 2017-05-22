a44512b4014271b04aa900af1a9e9ec34c7ceb11
pavelangelov <p_a_v_el@abv.bg>
2017-05-22 11:31:20 +0300

    Added comments.

--- Bg-Fishing/Bg-Fishing.Services/Contracts/IImageGalleryService.cs
+++ Bg-Fishing/Bg-Fishing.Services/Contracts/IImageGalleryService.cs
@@ -9,18 +9,52 @@ namespace Bg_Fishing.Services.Contracts
     {
         void Add(ImageGallery gallery);
 
+        /// <summary>
+        /// Get Image Gallery entity that match the Id.
+        /// </summary>
+        /// <param name="id">The Id of the gallery entity.</param>
+        /// <returns></returns>
         ImageGallery FindById(string id);
 
+        /// <summary>
+        /// Get all Galleries that contains images for passed lake name.
+        /// </summary>
+        /// <param name="lakeName">The name of the Lake.</param>
+        /// <returns>Returns <see cref="IEnumerable{ImageGalleryModel}"/></returns>
         IEnumerable<ImageGalleryModel> GetByLake(string lakeName);
 
+        /// <summary>
+        /// Add passed <see cref="Image"/> to gallery.
+        /// </summary>
+        /// <param name="image">The image.</param>
+        /// <param name="galleryId">The Id of the gallery.</param>
         void AddImageToGallery(Image image, string galleryId);
 
+        /// <summary>
+        /// Add passed <see cref="Image"/> to gallery.
+        /// </summary>
+        /// <param name="galleryName">The name of the gallery.</param>
+        /// <param name="image">The image.</param>
         void AddImageToGallery(string galleryName, Image image);
 
+        /// <summary>
+        /// Get all images from the gallery.
+        /// </summary>
+        /// <param name="galleryId">Gallery Id.</param>
+        /// <returns>Returns <see cref="IEnumerable{ImageModel}"/></returns>
         IEnumerable<ImageModel> GetAllImages(string galleryId);
 
-        IEnumerable<ImageModel> GetAllUnconfirmed();
+        /// <summary>
+        /// Get all unconfirmed images.
+        /// </summary>
+        /// <param name="galleryId">The Id of the gallery.</param>
+        /// <returns>Returns <see cref="IEnumerable{ImageModel}"/></returns>
+        IEnumerable<ImageModel> GetAllUnconfirmed(string galleryId);
 
+        /// <summary>
+        /// Save changes.
+        /// </summary>
+        /// <returns>Returns <see cref="System.Int32"/></returns>
         int Save();
     }
 }
