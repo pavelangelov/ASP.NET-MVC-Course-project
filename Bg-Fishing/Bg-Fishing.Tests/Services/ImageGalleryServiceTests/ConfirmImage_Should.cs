14c3b514463caba5ac24ae4d98893a4ef924f29c
pavelangelov <p_a_v_el@abv.bg>
2017-05-22 15:57:38 +0300

    Added new unit tests.

---
+++ Bg-Fishing/Bg-Fishing.Tests/Services/ImageGalleryServiceTests/ConfirmImage_Should.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.Data;
+using Bg_Fishing.Models.Galleries;
+using Bg_Fishing.Services;
+using Bg_Fishing.Tests.Services.Mocks;
+
+namespace Bg_Fishing.Tests.Services.ImageGalleryServiceTests
+{
+    [TestFixture]
+    public class ConfirmImage_Should
+    {
+        [Test]
+        public void SetImageIsConfirmed_ToTrue_IfIdMatch()
+        {
+            // Arrange
+            var mockedCollection = new List<Image>()
+            {
+                new Image(){ IsConfirmed = true },
+                new Image(){ IsConfirmed = false },
+                new Image(){ IsConfirmed = true },
+            };
+            var mockedDbSet = MockDbSet.Mock(mockedCollection.AsQueryable());
+            mockedDbSet.Setup(d => d.Find(It.IsAny<object[]>())).Returns<object[]>(ids => mockedCollection.FirstOrDefault(d => d.Id == ids[0].ToString()));
+
+            var mockedDbContext = new Mock<IDatabaseContext>();
+            mockedDbContext.Setup(c => c.Images).Returns(mockedDbSet.Object);
+            var service = new ImageGalleryService(mockedDbContext.Object);
+            var searchedImageId = mockedCollection[1].Id;
+
+            // Act
+            service.ConfirmImage(searchedImageId);
+
+            // Assert
+            Assert.IsTrue(mockedCollection[1].IsConfirmed);
+        }
+    }
+}
---
+++ Bg-Fishing/Bg-Fishing.Tests/Services/ImageGalleryServiceTests/GetGalleriesWithUnconfirmedImages_Should.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Moq;
+using NUnit.Framework;
+
+using Bg_Fishing.Data;
+using Bg_Fishing.Models.Galleries;
+using Bg_Fishing.Services;
+using Bg_Fishing.Tests.Services.Mocks;
+
+namespace Bg_Fishing.Tests.Services.ImageGalleryServiceTests
+{
+    [TestFixture]
+    public class GetGalleriesWithUnconfirmedImages_Should
+    {
+        [Test]
+        public void ReturnOnlyGalleries_ThatHaveUnconfirmedImages()
+        {
+            // Arrange
+            var mockedCollection = this.GetGalleries();
+            var mockedDbSet = MockDbSet.Mock(mockedCollection.AsQueryable());
+            var mockedDbContext = new Mock<IDatabaseContext>();
+            mockedDbContext.Setup(c => c.ImageGalleries).Returns(mockedDbSet.Object);
+
+            var service = new ImageGalleryService(mockedDbContext.Object);
+
+            // Act
+            var result = service.GetGalleriesWithUnconfirmedImages();
+
+            // Assert
+            Assert.IsTrue(result.Count() == 1);
+        }
+
+        private IList<ImageGallery> GetGalleries()
+        {
+            var collectionWithAllConfirmedImages = new List<Image>()
+            {
+                new Image(){ IsConfirmed = true},
+                new Image(){ IsConfirmed = true},
+                new Image(){ IsConfirmed = true}
+            };
+            var collectionWithUnconfirmedImages = new List<Image>()
+            {
+                new Image(){ IsConfirmed = true},
+                new Image(){ IsConfirmed = false},
+                new Image(){ IsConfirmed = true},
+            };
+
+            return new List<ImageGallery>()
+            {
+                new ImageGallery() { Images = collectionWithAllConfirmedImages },
+                new ImageGallery() { Images = collectionWithUnconfirmedImages },
+                new ImageGallery(){ Images = collectionWithAllConfirmedImages }
+            };
+        }
+    }
+}
